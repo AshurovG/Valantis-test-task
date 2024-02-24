70d2cc3b980e9d0de96bd725ab047d3ef1e4516b
Ashurov Georgy <ashurov1308@mail.ru>
2024-02-24 22:21:45 +0300

    add basic configurations

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import styles from './App.module.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className={styles.test}>
+        fdsf
+      </div>
+    </>
+  )
+}
+
+export default App
